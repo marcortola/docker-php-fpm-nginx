f826aa45784ba1b558d5d66fe538c76eb8b45ee9
marcortola <15958009+marcortola@users.noreply.github.com>
2022-11-14 11:14:51 +0100

    Fix auto envsubst

--- rootfs/usr/local/bin/docker-entrypoint.d/10-auto-envsubst.sh
+++ rootfs/usr/local/bin/docker-entrypoint.d/10-auto-envsubst.sh
@@ -8,9 +8,12 @@ filter="(FPM_|NGINX_|USER)"
 
 defined_envs=$(printf '${%s} ' $(awk "END { for (name in ENVIRON) { print ( name ~ /${filter}/ ) ? name : \"\" } }" </dev/null))
 
-find "$template_dirs" -type f -name "*$suffix" -print | while read -r template; do
-  file="${template%$suffix}"
-  envsubst "$defined_envs" <"$template" >"$file"
+echo "$template_dirs" | tr ' ' '\n' | while read -r path; do
+  find "$path" -type f -name "*$suffix" -print | while read -r template; do
+    file="${template%$suffix}"
+    rm -f "$file"
+    envsubst "$defined_envs" <"$template" >"$file"
+  done
 done
 
 exit 0
