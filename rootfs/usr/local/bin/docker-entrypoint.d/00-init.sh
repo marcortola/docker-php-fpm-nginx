2cf3cc1d1f885cd38f475255f0fa54031178bce4
marcortola <15958009+marcortola@users.noreply.github.com>
2022-12-14 17:50:48 +0100

    Fix tmp upload issues

--- rootfs/usr/local/bin/docker-entrypoint.d/00-init.sh
+++ rootfs/usr/local/bin/docker-entrypoint.d/00-init.sh
@@ -2,6 +2,10 @@
 
 set -eu
 
+mkdir -p /var/lib/nginx/tmp
 mkdir -p "$NGINX_CACHE_DIR"
 mkdir -p "$NGINX_FASTCGI_TMP_DIR"
+
+chown -R "$USER:$USER" /var/lib/nginx
+chmod -R 755 /var/lib/nginx
 chown -R "$USER:$USER" "$NGINX_FASTCGI_TMP_DIR"
